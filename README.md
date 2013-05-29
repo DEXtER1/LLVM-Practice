5e3ed476773b8a88206f61c1baba12633e885faf
Himanshu K. Shah <hshah@peace.CS.Princeton.EDU>
2013-05-29 12:33:34 -0400

    Files Cleanup

--- README.md
+++
@@ -1,4 +0,0 @@
-LLVM-Practice
-=============
-
-On the quest of Learning LLVM, all the practice programs will be stored under this single directory
--- sample/run.sh
+++
@@ -1,9 +0,0 @@
-if [ $1 = "d" ];then
-opt -profile-loader -load ~/llvm-liberty-objects/Debug+Asserts/lib/HKTailDuplication.so -HKtaildupl -debug program.bc -o prog.tail.bc
-fi
-if [ $1 = "n" ];then
-opt -profile-loader -load ~/llvm-liberty-objects/Debug+Asserts/lib/HKTailDuplication.so -HKtaildupl program.bc -o prog.tail.bc
-fi
-
-
-
